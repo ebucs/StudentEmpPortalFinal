f3e6937560c50b50eeaf6464785084e4c1eb2bef
Thandeka Madonsela <A0079417@wits.ac.za>
2023-06-23 13:18:16 +0200

    Uploading Application Document

---
+++ Areas/studentApplicationJ/Models/ApplicationDocument.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentEmploymentPortal.Areas.studentApplicationJ.Models
+{
+    public class ApplicationDocument
+    {
+        //ApplicationId
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string ApplicationDocumentId { get; set; }
+
+        //ApplicationId
+        [ForeignKey("ApplicationId")]
+        public string ApplicationId { get; set; }
+
+        [Required]
+        public string DocumentName { get; set; }
+
+        [Required]
+        public byte[] Documet { get; set; }
+
+    }
+}
